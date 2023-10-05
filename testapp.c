4e0c43e0049e08c9c04c7dc200daa195a99c4af4
kadensungbincho <kadensungbincho@linecorp.com>
2023-10-05 23:48:09 +0900

    feat: Add a test case in testapp.c

--- testapp.c
+++ testapp.c
@@ -50,6 +50,15 @@ main(int argc, char **argv){
     
     topo = build_first_topo();
 
+    
+    sleep(2);
+
+    node_t *snode = get_node_by_node_name(topo, "R0_re");
+    interface_t *oif = get_node_if_by_name(snode, "eth0/0");
+
+    char msg[] = "Hello, how are you\0";
+    send_pkt_out(msg, strlen(msg), oif);
+
     start_shell(); 
     return 0;
 }
